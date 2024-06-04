f0184c31b95ff4c6eee01c519ba9cde621260feb
ozdemirmt99 <ozdermirmt99@gmail.com>
2024-06-04 21:20:58 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Container from "./components/Container/Container";
 import CharactersList from "./components/List/CharactersList";
 import SearchArea from "./components/Search/SearchArea";
-import {getChracters} from "./connection/Stream";
+import { getChracters } from "./connection/Stream";
 
 function App() {
- let all = getChracters("ric")
-//  console.log(all);
+  const [allData, setAllData] = useState([]);
+  useEffect( () => {
+    const fetching= async()=>{
+      let all = await getChracters("ric");
+    setAllData(all);
+    }
+    fetching()
+  }, []);
+  //  let all = getChracters("ric")
+  //  console.log(all);
   return (
     <div className="App">
       <div className="App-header">
         <Container>
-          <SearchArea/>
-          <CharactersList/>
+          <SearchArea />
+          <CharactersList allChrachters={allData}/>
         </Container>
       </div>
     </div>
